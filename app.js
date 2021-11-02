8fc325a08937ecf98c2172c67c3675810539ae6a
Lao70 <estanislaocoloma@gmail.com>
2021-11-02 00:01:02 -0300

    comienzo de page detalle de producto

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.use(express.static("public"));
 app.listen(5000, () =>{
     console.log("Servidor funcionando en local host 5000");
 })
-app.get('/', (req, res)=>{
+
+/*app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/views/index.html');
+});  
+*/
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/views/detalle_del_producto.html');
 });
