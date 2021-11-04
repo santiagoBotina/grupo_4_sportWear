0370ac65fe993bf426010efae828bc0f14b79b61
maxi852 <maxi.rocca99@gmail.com>
2021-11-04 12:24:56 -0300

    carrito de compras

--- app.js
+++ app.js
@@ -7,11 +7,19 @@ app.listen(5000, () =>{
     console.log("Servidor funcionando en local host 5000");
 })
 
-/*app.get('/', (req, res)=>{
+/*
+app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/views/index.html');
 });  
 */
 
+/*
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/views/detalle_del_producto.html');
 });
+*/
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/views/carrito.html');
+});
+
