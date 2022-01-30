69f3e7d9064f723f9c9dba886bbba5930c0d9757
santiagoBotina <santiagoboe04@gmail.com>
2022-01-30 14:44:22 -0500

    Arreglando el display de los errores en el login con express-validator

--- src/controllers/controllerLogin.js
+++ src/controllers/controllerLogin.js
@@ -16,21 +16,34 @@ const loginController = {
       let usuarioLogged;
 
       for (let i = 0; i < users.length; i++) {
-        if (req.body.email == users[i].email) {
-          if (bcrypt.compareSync(req.body.password, users[i].password)) {
-            usuarioLogged = users[i];
-          }
+        if (
+          req.body.email == users[i].email &&
+          bcrypt.compareSync(req.body.password, users[i].password)
+        ) {
+          usuarioLogged = users[i];
         }
       }
       req.session.usuarioLogged = usuarioLogged;
+
+      if (usuarioLogged == undefined) {
+        return res.render("login", {
+          errors: {
+            password: {
+              msg: "Las credenciales son inválidas",
+            },
+          },
+        });
+      }
+
+      res.send("Hola " + usuarioLogged.name);
 
       if (req.body.recordame != undefined) {
         res.cookie("recordar", usuarioLogged.email, { maxAge: 120000 });
       }
 
-      res.send("Hola " + req.session.usuarioLogged.name);
+      // res.send("Hola " + req.session.usuarioLogged.name);
     } else {
-      return res.render("login", { errors: errores.array() });
+      return res.render("login", { errors: errores.mapped() });
     }
   },
 };
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const loginController = require("../controllers/controllerLogin.js");
 const router = express.Router();
-const { body, check } = require("express-validator");
+const { body } = require("express-validator");
 const isLoggedMiddleware = require("../middlewares/loggedMiddleware");
 
 let loginValidator = [
-  body("email")
-    .notEmpty()
-    .isEmail()
-    .withMessage("Debes llenar el campo de email"),
-  body("password")
-    .notEmpty()
-    .withMessage("Debes llenar el campo de contraseña"),
+  body("email").notEmpty().withMessage("Debes llenar este campo"),
+  body("password").notEmpty().withMessage("Debes llenar este campo"),
 ];
 
 router.get("/", isLoggedMiddleware, loginController.loginRoute);
