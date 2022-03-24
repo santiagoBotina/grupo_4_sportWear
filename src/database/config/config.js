b046f4d8a7725ec3ae3dc3c643c08ad4d25980cd
santiagoBotina <santiagoboe04@gmail.com>
2022-03-23 23:10:45 -0500

    seteando config de nuevo con use_env_variable

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -7,10 +7,7 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "aqllpwi2wmacmkeg	",
-    password: "pu2o2j5u5f3zcvrt",
-    database: "wlv3dx1q8t9q5x0f",
-    host: "dt3bgg3gu6nqye5f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+    use_env_variable: "JAWSDB_URL",
     dialect: "mysql",
   },
 };
