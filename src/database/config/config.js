53648bc4c9fd04b395eeaccec6f665af3c50a09c
santiagoBotina <santiagoboe04@gmail.com>
2022-03-23 22:37:05 -0500

    seteando base de datos de nuevo

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,23 @@
 module.exports = {
-  username: "b026931d86074d",
-  password: "cc77deaa",
-  database: "heroku_92378982fd73705",
-  host: "us-cdbr-east-05.cleardb.net",
-  dialect: "mysql",
+  development: {
+    username: "b026931d86074d",
+    password: "cc77deaa",
+    database: "heroku_92378982fd73705",
+    host: "us-cdbr-east-05.cleardb.net",
+    dialect: "mysql",
+  },
+  test: {
+    username: "b026931d86074d",
+    password: "cc77deaa",
+    database: "heroku_92378982fd73705",
+    host: "us-cdbr-east-05.cleardb.net",
+    dialect: "mysql",
+  },
+  production: {
+    username: "b026931d86074d",
+    password: "cc77deaa",
+    database: "heroku_92378982fd73705",
+    host: "us-cdbr-east-05.cleardb.net",
+    dialect: "mysql",
+  },
 };
