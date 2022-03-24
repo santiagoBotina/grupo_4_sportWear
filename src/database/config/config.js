d3d3b604cc8d3f2e73c4a286c471fe8663a0a0f3
santiagoBotina <santiagoboe04@gmail.com>
2022-03-23 22:22:29 -0500

    seteando base de datos para heroku y arreglando error de config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,20 +1,20 @@
 module.exports = {
   development: {
-    username: "b026931d86074d:cc77deaa",
+    username: "b026931d86074d",
     password: "cc77deaa",
     database: "heroku_92378982fd73705",
     host: "us-cdbr-east-05.cleardb.net",
     dialect: "mysql",
   },
   test: {
-    username: "b026931d86074d:cc77deaa",
+    username: "b026931d86074d",
     password: "cc77deaa",
     database: "heroku_92378982fd73705",
     host: "us-cdbr-east-05.cleardb.net",
     dialect: "mysql",
   },
   production: {
-    username: "b026931d86074d:cc77deaa",
+    username: "b026931d86074d",
     password: "cc77deaa",
     database: "heroku_92378982fd73705",
     host: "us-cdbr-east-05.cleardb.net",
