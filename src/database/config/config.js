8cb82b54f9e7dbc420a024522a234a085aa94c1a
santiagoBotina <santiagoboe04@gmail.com>
2022-03-23 23:15:57 -0500

    seteando config de nuevo con jaws de heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -7,7 +7,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    use_env_variable: "JAWSDB_URL",
+    username: "aqllpwi2wmacmkeg	",
+    password: "pu2o2j5u5f3zcvrt",
+    database: "wlv3dx1q8t9q5x0f",
+    host: "dt3bgg3gu6nqye5f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
     dialect: "mysql",
   },
 };
