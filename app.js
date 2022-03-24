73f776ae8b4303d03643c222452fba0b35b06db0
santiagoBotina <santiagoboe04@gmail.com>
2022-03-23 21:40:56 -0500

    agregando archivo Procfile y seteando puertos

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use("/usuario", routerUsers);
 app.use("/api/users", routerApiUsers);
 app.use("/api/products", routerApiProducts);
 
-app.listen(5000, () => {
-  console.log("Servidor funcionando en local host 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando en el puerto ${PORT}`);
 });
