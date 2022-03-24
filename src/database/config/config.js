e6d2c3d8bcdec695dd009bdb60863597be973110
santiagoBotina <santiagoboe04@gmail.com>
2022-03-23 22:59:33 -0500

    Usando jaws para deploy

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,16 @@
 module.exports = {
   development: {
-    username: "b026931d86074d",
-    password: "cc77deaa",
-    database: "heroku_92378982fd73705",
-    host: "us-cdbr-east-05.cleardb.net",
-    dialect: "mysql",
-  },
-  test: {
-    username: "b026931d86074d",
-    password: "cc77deaa",
-    database: "heroku_92378982fd73705",
-    host: "us-cdbr-east-05.cleardb.net",
+    username: "root",
+    password: null,
+    database: "sport_wear",
+    host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
-    username: "b026931d86074d",
-    password: "cc77deaa",
-    database: "heroku_92378982fd73705",
-    host: "us-cdbr-east-05.cleardb.net",
+    username: "aqllpwi2wmacmkeg	",
+    password: "pu2o2j5u5f3zcvrt",
+    database: "wlv3dx1q8t9q5x0f",
+    host: "dt3bgg3gu6nqye5f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
     dialect: "mysql",
   },
 };
