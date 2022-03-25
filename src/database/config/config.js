84856316cd390018141504b76ff6348e4f522745
santiagoBotina <santiagoboe04@gmail.com>
2022-03-24 21:22:17 -0500

    agregando configuracion de la base de datos

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,16 +1,16 @@
 module.exports = {
   development: {
-    username: "root",
-    password: null,
-    database: "sport_wear",
-    host: "127.0.0.1",
+    username: "b7001e6dab4689",
+    password: "cb10e5d1",
+    database: "heroku_98d9d176e135adb",
+    host: "us-cdbr-east-05.cleardb.net",
     dialect: "mysql",
   },
   production: {
-    username: "aqllpwi2wmacmkeg	",
-    password: "pu2o2j5u5f3zcvrt",
-    database: "wlv3dx1q8t9q5x0f",
-    host: "dt3bgg3gu6nqye5f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+    username: "b7001e6dab4689",
+    password: "cb10e5d1",
+    database: "heroku_98d9d176e135adb",
+    host: "us-cdbr-east-05.cleardb.net",
     dialect: "mysql",
   },
 };
